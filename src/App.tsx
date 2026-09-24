6d8e2e7844c08826e0859fbca0dc15ebf053729e
Sabir Kazi <sabirkazi135@gmail.com>
2026-09-25 02:42:55 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,83 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { useState } from "react";
+import type { ChangeEvent } from "react";
+
+type ProductCardProps = {
+  product: Product;
+  onSelect: (product: Product) => void;
+};
+
+type Product = {
+  name: string;
+  price: number;
+  discount?: number;
+  category: "electronics" | "clothing" | "food";
+};
+
+const product1: Product = {
+  name: "Samsung M52",
+  price: 25000,
+  discount: 10,
+  category: "electronics",
+};
+
+const product2: Product = {
+  name: "Samsung M55",
+  price: 35000,
+  category: "electronics",
+};
+
+const products: Product[] = [product1, product2];
+
+function ProductCard({ product, onSelect }: ProductCardProps) {
+  return (
+    <div onClick={() => onSelect(product)}>
+      <h2>{product.name}</h2>
+      <p>₹{product.price}</p>
+
+      {product.discount !== undefined ? (
+        <p>Discount: {product.discount}%</p>
+      ) : null}
+
+      <p>{product.category}</p>
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(search.toLowerCase()),
+  );
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <input
+        type="text"
+        value={search}
+        onChange={handleSearch}
+        placeholder="Search products..."
+      />
 
-      <div className="ticks"></div>
+      {filteredProducts.map((product) => (
+        <ProductCard
+          key={product.name}
+          product={product}
+          onSelect={setSelectedProduct}
+        />
+      ))}
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      {selectedProduct ? (
+        <div>Selected product: {selectedProduct.name}</div>
+      ) : null}
     </>
-  )
+  );
 }
 
-export default App
+export default App;

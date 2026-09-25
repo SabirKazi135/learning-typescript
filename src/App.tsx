d0f6b2b2562381cd558b8213ffe4d85ea98d7b87
Sabir Kazi <sabirkazi135@gmail.com>
2026-09-25 21:02:05 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,37 @@
-import { useState } from "react";
-import type { ChangeEvent } from "react";
+import type { ReactNode } from "react";
 
-type ProductCardProps = {
-  product: Product;
-  onSelect: (product: Product) => void;
+type CardProps = {
+  children: ReactNode;
 };
 
-type Product = {
-  name: string;
-  price: number;
-  discount?: number;
-  category: "electronics" | "clothing" | "food";
-};
-
-const product1: Product = {
-  name: "Samsung M52",
-  price: 25000,
-  discount: 10,
-  category: "electronics",
-};
-
-const product2: Product = {
-  name: "Samsung M55",
-  price: 35000,
-  category: "electronics",
-};
-
-const products: Product[] = [product1, product2];
-
-function ProductCard({ product, onSelect }: ProductCardProps) {
-  return (
-    <div onClick={() => onSelect(product)}>
-      <h2>{product.name}</h2>
-      <p>₹{product.price}</p>
-
-      {product.discount !== undefined ? (
-        <p>Discount: {product.discount}%</p>
-      ) : null}
-
-      <p>{product.category}</p>
-    </div>
-  );
+function Card({ children }: CardProps) {
+  return <div>{children}</div>;
 }
 
+function Panel({ children }: CardProps) {
+  return <div>{children}</div>;
+}
 function App() {
-  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
-
-  const [search, setSearch] = useState("");
-
-  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.target.value);
-  };
-
-  const filteredProducts = products.filter((product) =>
-    product.name.toLowerCase().includes(search.toLowerCase()),
-  );
-
   return (
-    <>
-      <input
-        type="text"
-        value={search}
-        onChange={handleSearch}
-        placeholder="Search products..."
-      />
+    <div>
+      <p>Hello</p>
 
-      {filteredProducts.map((product) => (
-        <ProductCard
-          key={product.name}
-          product={product}
-          onSelect={setSelectedProduct}
-        />
-      ))}
+      <Card>
+        <h2>Samsung M52</h2>
+        <p>₹25,000</p>
+      </Card>
 
-      {selectedProduct ? (
-        <div>Selected product: {selectedProduct.name}</div>
-      ) : null}
-    </>
+      <Panel>
+        <h2>Samsung M52</h2>
+        <p>₹25,000</p>
+        <p>Electronics</p>
+      </Panel>
+
+      <Panel>
+        <h2>User Profile</h2>
+        <p>Sabir</p>
+      </Panel>
+    </div>
   );
 }
 
